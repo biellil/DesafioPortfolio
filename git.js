6dc591c2e6ac00c35541af4defbd578f3771ed36
biel.lil <78098857+biellil@users.noreply.github.com>
2023-03-14 16:15:55 -0300

    14/03

--- git.js
+++ git.js
@@ -1,35 +1,22 @@
-const username = 'biellil';
+const url = "https://api.github.com/users/biellil/repos?sort=updated";
+const username = "biellil";
 
-function getRepositories(username, callback) {
-    const url = `https://api.github.com/users/${username}/repos`;
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        let repos = [];
-        if (Array.isArray(data)) {
-          repos = data.map(repo => ({
-            name: repo.name,
-            url: repo.html_url
-          }));
-        }
-        callback(repos);
-      })
-      .catch(error => console.error(error));
-  }
-  
-  function showRepositories(repos) {
-    const reposList = document.getElementById('repos-list');
-    reposList.innerHTML = '';
-    repos.forEach(repo => {
-      const listItem = document.createElement('li');
-      const link = document.createElement('a');
-      link.href = repo.url;
-      link.textContent = repo.name;
-      listItem.appendChild(link);
-      reposList.appendChild(listItem);
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    const latestRepos = data.slice(0, 2);
+
+    latestRepos.forEach((latestRepo, index) => {
+      const repoName = latestRepo.name;
+      const language = latestRepo.language;
+      const repoUrl = latestRepo.html_url;
+
+      const reposList = document.querySelector(`.repos-list${index + 1}`);
+      const repoItem = document.createElement("div");
+      repoItem.innerHTML = `
+      <a href="${repoUrl}">${repoName}</a>
+       <h3> ${language}</h3>`;
+      reposList.appendChild(repoItem);
     });
-  }
-  
- 
-  getRepositories(username, showRepositories);
-  
+  })
+  .catch((error) => console.error(error));
